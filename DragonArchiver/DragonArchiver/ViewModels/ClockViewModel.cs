8b2e0097d4c092649a0e45bfc03bde62117d88d2
ipike2 <ipike2@mail.lakelandcc.edu>
2022-04-13 21:50:10 -0400

    Created basic structure of application

---
+++ DragonArchiver/DragonArchiver/ViewModels/ClockViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel;
+
+namespace DragonArchiver.ViewModels
+{
+    public class ClockViewModel : INotifyPropertyChanged
+    {
+        DateTime dateTime;
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public DateTime DateTime
+        {
+            get
+            {
+                return dateTime;
+            }
+            set
+            {
+                if (dateTime != value)
+                {
+                    dateTime = value;
+                    if (PropertyChanged != null)
+                    {
+                        PropertyChanged(this, new PropertyChangedEventArgs("DateTime"));
+                    }
+                }
+            }
+        }
+
+        public ClockViewModel()
+        {
+            this.DateTime = DateTime.Now;
+
+            // Update the DateTime property every second.
+            Timer timer = new Timer(new TimerCallback((s) => this.DateTime = DateTime.Now), null, 0, 1000);
+        }
+    }
+}
--- docs/README.md
+++
@@ -1,2 +0,0 @@
-# DragonArchiver
-Temp file
