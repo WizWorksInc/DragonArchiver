119fee59e0e966367223ce3f8fe91a9cbb61d86e
Ian Pike <Ianpike98@gmail.com>
2022-05-25 22:05:00 -0400

    Create some of the initial class design

---
+++ DragonArchiver.Core/Model/NPC.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonArchiver.Core.Model
+{
+    internal class NPC : ICreature, INonPlayerCharacter, ILocation
+    {
+
+    }
+}
