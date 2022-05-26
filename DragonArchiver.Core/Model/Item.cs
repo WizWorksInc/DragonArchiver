b084f96dc8b7d3f97cb7be3ac5e6c639dd819039
Ian Pike <Ianpike98@gmail.com>
2022-05-26 18:44:40 -0400

    Adding more classes and testing files

---
+++ DragonArchiver.Core/Model/Item.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DragonArchiver.Core.Model
+{
+    internal class Item
+    {
+        public string Name { get; set; }
+        public char Type { get; set; }
+        public string Rarity { get; set; }
+        public string Value { get; set; }
+        public string Weight { get; set; }
+        public string Source { get; set; }
+        public uint Page { get; set; }
+        public List<object> Entries { get; set; }
+        
+        public string WeaponCategory { get; set; }
+        public bool RequiresAttunement { get; set; }
+
+
+    }
+}
