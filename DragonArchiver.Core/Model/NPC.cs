912fcfc65517068135c70c66188c06da8b2e4c3c
Ian Pike <Ianpike98@gmail.com>
2022-05-27 20:28:07 -0400

    Small adjustments

--- DragonArchiver.Core/Model/NPC.cs
+++ DragonArchiver.Core/Model/NPC.cs
@@ -1,6 +1,6 @@
 ﻿namespace DragonArchiver.Core.Model
 {
-    internal class NPC : Creature, ILocations
+    internal class Npc : Creature, ILocations
     {
         public override string Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
