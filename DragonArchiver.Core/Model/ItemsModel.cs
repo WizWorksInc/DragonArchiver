6bb5d9ffeb236cba9133e102c64cc952fa331b25
Ian Pike <Ianpike98@gmail.com>
2022-05-28 04:39:58 -0400

    Working on data binding

--- DragonArchiver.Core/Model/ItemsModel.cs
+++ DragonArchiver.Core/Model/ItemsModel.cs
@@ -1,6 +1,22 @@
-﻿namespace DragonArchiver.Core.Model;
+﻿using System.Collections.ObjectModel;
+using CommunityToolkit.Mvvm.ComponentModel;
 
-public class ItemsModel
+namespace DragonArchiver.Core.Model;
+
+public class ItemsModel : ObservableObject
 {
-    
+
+    private Item testItem = new Item("slugtest", "nametest", "typetest", "descriptiontest", "raritytest",
+        "attunementtest", "documentslugtest", "documenttitletest");
+
+    private readonly Item _item;
+
+    public ItemsModel(Item item) => this._item = item;
+
+    public string Name
+    {
+        get => _item.Name;
+        set => SetProperty(_item.Name, value, _item, (u, n) => u.Name = n);
+    }
+
 }
