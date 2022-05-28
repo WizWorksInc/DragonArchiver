ce285584e49635eebf913e03b0c72b0f8a90df5e
Ian Pike <Ianpike98@gmail.com>
2022-05-28 00:26:39 -0400

    Bunch of changes

--- DragonArchiver.Core/Model/Item.cs
+++ DragonArchiver.Core/Model/Item.cs
@@ -7,38 +7,24 @@ namespace DragonArchiver.Core.Model;
 /// </summary>
 internal class Item
 {
-    /// <summary>
-    /// The constructor is mostly here for our deserialization of JSON.
-    /// We treat all of our values as optional due to the fact that some fields
-    /// may be empty as not every item has all of these as an option.
-    /// We also check for NULL values.
-    /// </summary>
-    /// <param name="name"></param>
-    /// <param name="type"></param>
-    /// <param name="rarity"></param>
-    /// <param name="value"></param>
-    /// <param name="source"></param>
-    /// <param name="page"></param>
-    /// <param name="entries"></param>
-    /// <exception cref="ArgumentNullException"></exception>
-    public Item([NotNull] string name = null, [NotNull] string type = null, [NotNull] string rarity = null,
-        [NotNull] string value = null, [NotNull] string source = null, int page = default,
-        [NotNull] List<string> entries = null)
+    public Item(string slug, string name, string type, string desc, string rarity, string requiresAttunement, string documentSlug, string documentTitle)
     {
-        Name = name ?? throw new ArgumentNullException(nameof(name));
-        Type = type ?? throw new ArgumentNullException(nameof(type));
-        Rarity = rarity ?? throw new ArgumentNullException(nameof(rarity));
-        Value = value ?? throw new ArgumentNullException(nameof(value));
-        Source = source ?? throw new ArgumentNullException(nameof(source));
-        Page = page;
-        Entries = entries ?? throw new ArgumentNullException(nameof(entries));
+        Slug = slug;
+        Name = name;
+        Type = type;
+        Desc = desc;
+        Rarity = rarity;
+        RequiresAttunement = requiresAttunement;
+        DocumentSlug = documentSlug;
+        DocumentTitle = documentTitle;
     }
 
+    public string Slug { get; set; }
     public string Name { get; set; }
     public string Type { get; set; }
+    public string Desc { get; set; }
     public string Rarity { get; set; }
-    public string Value { get; set; }
-    public string Source { get; set; }
-    public int Page { get; set; }
-    public List<string> Entries { get; set; }
+    public string RequiresAttunement { get; set; }
+    public string DocumentSlug { get; set; }
+    public string DocumentTitle { get; set; }
 }
---
+++ DragonArchiver.Utilities/Class1.cs
@@ -0,0 +1,7 @@
+﻿namespace DragonArchiver.Utilities
+{
+    public class Class1
+    {
+
+    }
+}
