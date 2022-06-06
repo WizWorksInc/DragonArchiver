9af183899d1fba8de76fb229fa74ec28c64ce7f4
Ian Pike <Ianpike98@gmail.com>
2022-06-06 02:09:41 -0400

    Create Spell.cs

---
+++ DragonArchiver.Core/Model/Spell.cs
@@ -0,0 +1,26 @@
+namespace DragonArchiver.Core.Model
+{
+    public class Spell
+    {
+        public string name { get; set; }
+        public string desc { get; set; }
+        public string higher_level { get; set; }
+        public string page { get; set; }
+        public string range { get; set; }
+        public string components { get; set; }
+        public string material { get; set; }
+        public string ritual { get; set; }
+        public string duration { get; set; }
+        public string concentration { get; set; }
+        public string casting_time { get; set; }
+        public string level { get; set; }
+        public int level_int { get; set; }
+        public string school { get; set; }
+        public string @class { get; set; }
+        public string archetype { get; set; }
+        public string circles { get; set; }
+        public string domains { get; set; }
+        public string oaths { get; set; }
+        public string patrons { get; set; }
+    }
+}
