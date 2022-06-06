7b4a3cc5102cc6c379d88e2a22c34b814d52d622
Ian Pike <Ianpike98@gmail.com>
2022-06-06 03:08:26 -0400

    Update Player.cs

--- DragonArchiver.Core/Model/Player.cs
+++ DragonArchiver.Core/Model/Player.cs
@@ -1,6 +1,6 @@
 ﻿namespace DragonArchiver.Core.Model
 {
-    internal class Player : Creature, IClassable
+    internal class Player : Creature
     {
         public override string Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
         public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
