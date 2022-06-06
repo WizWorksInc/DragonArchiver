96461ade2fed1e090e6b518015ae6628d25c8038
Ian Pike <Ianpike98@gmail.com>
2022-06-06 01:50:57 -0400

    Create Testfileforcreaturesandmonsters.cs
    This is to be deleted later but is here for reference.

---
+++ DragonArchiver.Core/Model/Testfileforcreaturesandmonsters.cs
@@ -0,0 +1,100 @@
+// Root myDeserializedClass = JsonConvert.DeserializeObject<List<Root>>(myJsonResponse);
+    public class Action
+    {
+        public string name { get; set; }
+        public string desc { get; set; }
+        public int attack_bonus { get; set; }
+        public string damage_dice { get; set; }
+        public int? damage_bonus { get; set; }
+    }
+
+    public class LegendaryAction
+    {
+        public string name { get; set; }
+        public string desc { get; set; }
+        public int attack_bonus { get; set; }
+        public string damage_dice { get; set; }
+    }
+
+    public class Reaction
+    {
+        public string name { get; set; }
+        public string desc { get; set; }
+        public int attack_bonus { get; set; }
+    }
+
+    public class Root
+    {
+        public string name { get; set; }
+        public string size { get; set; }
+        public string type { get; set; }
+        public string subtype { get; set; }
+        public string alignment { get; set; }
+        public int armor_class { get; set; }
+        public int hit_points { get; set; }
+        public string hit_dice { get; set; }
+        public string speed { get; set; }
+        public int strength { get; set; }
+        public int dexterity { get; set; }
+        public int constitution { get; set; }
+        public int intelligence { get; set; }
+        public int wisdom { get; set; }
+        public int charisma { get; set; }
+        public int constitution_save { get; set; }
+        public int intelligence_save { get; set; }
+        public int wisdom_save { get; set; }
+        public int history { get; set; }
+        public int perception { get; set; }
+        public string damage_vulnerabilities { get; set; }
+        public string damage_resistances { get; set; }
+        public string damage_immunities { get; set; }
+        public string condition_immunities { get; set; }
+        public string senses { get; set; }
+        public string languages { get; set; }
+        public string challenge_rating { get; set; }
+        public List<SpecialAbility> special_abilities { get; set; }
+        public List<Action> actions { get; set; }
+        public string legendary_desc { get; set; }
+        public List<LegendaryAction> legendary_actions { get; set; }
+        public SpeedJson speed_json { get; set; }
+        public string armor_desc { get; set; }
+        public int? medicine { get; set; }
+        public int? religion { get; set; }
+        public List<string> spells { get; set; }
+        public int? dexterity_save { get; set; }
+        public int? charisma_save { get; set; }
+        public int? stealth { get; set; }
+        public string group { get; set; }
+        public int? persuasion { get; set; }
+        public int? insight { get; set; }
+        public int? deception { get; set; }
+        public int? arcana { get; set; }
+        public int? athletics { get; set; }
+        public int? acrobatics { get; set; }
+        public int? strength_save { get; set; }
+        public List<Reaction> reactions { get; set; }
+        public int? survival { get; set; }
+        public int? investigation { get; set; }
+        public int? nature { get; set; }
+        public int? intimidation { get; set; }
+        public int? performance { get; set; }
+    }
+
+    public class SpecialAbility
+    {
+        public string name { get; set; }
+        public string desc { get; set; }
+        public int attack_bonus { get; set; }
+        public string damage_dice { get; set; }
+    }
+
+    public class SpeedJson
+    {
+        public int walk { get; set; }
+        public int swim { get; set; }
+        public int? fly { get; set; }
+        public int? burrow { get; set; }
+        public int? climb { get; set; }
+        public bool? hover { get; set; }
+        public string notes { get; set; }
+    }
