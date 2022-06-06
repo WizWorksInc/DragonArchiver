5093d6d8e116b4de7e8b8388bfc258d9777b0715
Ian Pike <Ianpike98@gmail.com>
2022-06-06 02:43:35 -0400

    Update Item.cs

--- DragonArchiver.Core/Model/Item.cs
+++ DragonArchiver.Core/Model/Item.cs
@@ -8,9 +8,9 @@ namespace DragonArchiver.Core.Model;
 public class Item
 {
 
-    public string name { get; set; }
-    public string desc { get; set; }
-    public string type { get; set; }
-    public string rarity { get; set; }
+    public string? name { get; set; }
+    public string? desc { get; set; }
+    public string? type { get; set; }
+    public string? rarity { get; set; }
 
 }
