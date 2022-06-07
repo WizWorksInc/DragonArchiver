c25000a5e036fde37ac4644896a23d4a80bded84
Ian Pike <Ianpike98@gmail.com>
2022-06-07 18:59:43 -0400

    Create ItemService.cs

---
+++ DragonArchiver.Core/Services/ItemService.cs
@@ -0,0 +1,36 @@
+using System.Net.Http.Json;
+
+namespace DragonArchiver.Core.Services;
+
+public class ItemService
+{
+    HttpClient httpClient;
+    private string _url;
+    private string _uri = @"DragonArchiver.Core/Data/JsonData/WOTC_5e_SRD_v5.1";
+
+    public ItemService()
+    {
+        this.httpClient = new HttpClient();
+    }
+
+    List<Item> itemList;
+    public async Task<List<Item>> GetItems()
+    {
+        if (itemList?.Count > 0)
+            return itemList;
+
+        // Online
+        /*var response = await httpClient.GetAsync("");
+        if (response.IsSuccessStatusCode)
+        {
+            itemList = await response.Content.ReadFromJsonAsync<List<Item>>();
+        }*/
+        // Offline
+        using var stream = await FileSystem.OpenAppPackageFileAsync(_uri + "/magicitems.json");
+        using var reader = new StreamReader(stream);
+        var contents = await reader.ReadToEndAsync();
+        itemList = JsonSerializer.Deserialize<List<Item>>(contents);
+        
+        return itemList;
+    }
+}
