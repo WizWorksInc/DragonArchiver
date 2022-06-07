fe839b5d0c4f705c64a72212e8dbdd84e8710c7b
Ian Pike <Ianpike98@gmail.com>
2022-06-06 21:04:21 -0400

    Oops forgot two classes

--- DragonArchiver.Core/Model/NPC.cs
+++ DragonArchiver.Core/Model/NPC.cs
@@ -2,25 +2,44 @@
 {
     internal class Npc : Creature
     {
-        public override string Id { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override string Name { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override object Type { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override object Size { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override object Alignment { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override Dictionary<string, ushort> Stats { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override ushort Speed { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override ushort HP { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override byte AC { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override List<string> Languages { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override bool CanSpeak { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override Dictionary<string, byte> SavingThrows { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override List<string> Conditions { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override List<string> ConditionImmunities { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override Dictionary<string, ushort> Senses { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override List<string> Immunities { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override List<string> Resistances { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override List<string> Vulnerabilities { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override Dictionary<string, object> Actions { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
-        public override Dictionary<string, ushort> Perceptions { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+	    public override string? Name { get; set; }
+	    public override string? Size { get; set; }
+	    public override string? Type { get; set; }
+	    public override string? Subtype { get; set; }
+	    public override string? Alignment { get; set; }
+	    public override int ArmorClass { get; set; }
+	    public override int HitPoints { get; set; }
+	    public override string? HitDice { get; set; }
+	    public override string? Speed { get; set; }
+	    public override int Strength { get; set; }
+	    public override int Dexterity { get; set; }
+	    public override int Constitution { get; set; }
+	    public override int Intelligence { get; set; }
+	    public override int Wisdom { get; set; }
+	    public override int Charisma { get; set; }
+	    public override int? Acrobatics { get; set; }
+	    public override int? Arcana { get; set; }
+	    public override int? Athletics { get; set; }
+	    public override int? Deception { get; set; }
+	    public override int History { get; set; }
+	    public override int? Insight { get; set; }
+	    public override int? Intimidation { get; set; }
+	    public override int? Investigation { get; set; }
+	    public override int? Medicine { get; set; }
+	    public override int? Nature { get; set; }
+	    public override int Perception { get; set; }
+	    public override int? Performance { get; set; }
+	    public override int? Persuasion { get; set; }
+	    public override int? Religion { get; set; }
+	    public override int? Stealth { get; set; }
+	    public override int? Survival { get; set; }
+	    public override int? StrengthSave { get; set; }
+	    public override int? DexteritySave { get; set; }
+	    public override int ConstitutionSave { get; set; }
+	    public override int IntelligenceSave { get; set; }
+	    public override int WisdomSave { get; set; }
+	    public override int? CharismaSave { get; set; }
+	    public override string? Senses { get; set; }
+	    public override string? Languages { get; set; }
     }
 }
