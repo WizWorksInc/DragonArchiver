3d29c0c2e3617f9245f557f9880e6ba97b61bece
Ian Pike <Ianpike98@gmail.com>
2022-06-09 16:10:40 -0400

    Improved DiceService and updated unit tests

--- DragonArchiver.Core/Services/DiceService.cs
+++ DragonArchiver.Core/Services/DiceService.cs
@@ -1,6 +1,5 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
 using System.Linq;
 using System.Text.RegularExpressions;
 
@@ -11,85 +10,99 @@ namespace DragonArchiver.Core.Services;
 /// </summary>
 public class DiceService
 {
-     private readonly Random _rand = new Random();
-     
-     /// <summary>
-     /// Roll a Die!
-     /// </summary>
-     /// <returns>int</returns>
-     public int RollDice(int dieFaces)
-     {
-          return dieFaces == 0 ? 0 : _rand.Next(1, dieFaces);
-     }
+    private readonly Random _random;
 
-     /// <summary>
-     /// Roll multiple Dice!
-     /// </summary>
-     /// <param name="diceFaces"></param>
-     /// <param name="amountOfDice"></param>
-     /// <returns>List of dice rolls</returns>
-     public List<int> RollDice(int amountOfDice, int diceFaces )
-     {
-          // Validate we are receiving a valid entry otherwise return 0
-          if (amountOfDice <= 0 || diceFaces <= 0)
-               return new List<int>() {0};
-          
-          var returnList = new List<int>();
-          for (var i = 0; i < amountOfDice; i++)
-               returnList.Add(_rand.Next(1, diceFaces));
-          
-          return returnList;
-     }
+    /// <summary>
+    /// Accept a new instance of random to help with mocking and testing.
+    /// </summary>
+    /// <param name="random"></param>
+    public DiceService(Random random)
+    {
+        _random = random;
+    }
 
-     /// <summary>
-     /// Convert a hitpoint dice string to an integer using Regular Expressions.
-     /// </summary>
-     /// <param name="diceHitpointString"></param>
-     /// <returns>summed amount of hitpoint dice</returns>
-     public int ConvertHitpointDiceToIntegerAndSum(string diceHitpointString)
-     {
-          /* The following string: (\d+)d(\d+)([\+-]\d+)?$
-           * is a Regular Expression.
-           *
-           * The string is breaking down a hitpoint dice value into its bare components.
-           * For example:
-           *        We receive a string "1d20"
-           *        We call Regex.Match() and feed it our string and regular expression pattern
-           *        The string is broken down into 3 groups as such:
-           *             (Group1)(Group2)(Group3) == (1)(d)(20)
-           *
-           *        We then store this information into diceMatch
-           */
-          // 
-          Match diceMatch = Regex.Match(diceHitpointString, @"(\d+)d(\d+)([\+-]\d+)?$");
-          try
-          {
-               /* 
-                This expression validates that the string we received is valid integers
-                For example: 
-                    if we were to receive the following string 1d20
-                    we would call diceMatch to pull the value from group 1 which in this case is 1
-                    and also call group 2 which in this case is 20.
-                    For reference the string breaks down even further as this:
-                         (1)[d](20)
-                         where:
-                              diceMatch.Groups[1].Value == "1"
-                              diceMatch.Groups[2].Value == "20"
-                */
-               if (int.TryParse(diceMatch.Groups[1].Value, out int amountOfDice) &&
-                   int.TryParse(diceMatch.Groups[2].Value, out int typeOfDie))
-               {
-                    return RollDice(amountOfDice, typeOfDie).Sum();
-               }
-          }
-          catch (Exception e)
-          {
-               Debug.WriteLine(e);
-               throw;
-          }
+    /// <summary>
+    /// Roll a Die!
+    /// </summary>
+    /// <remarks>
+    /// We really should never roll a die exclusively as all JSON that
+    /// is receive should be properly formatted in the 1d20 notation.
+    /// </remarks>
+    /// <returns>int</returns>
+    private int RollDie(int dieFaces) => dieFaces <= 0 ? 0 : _random.Next(1, dieFaces);
 
-          return 0;
+    /// <summary>
+    /// Roll Dice!
+    /// </summary>
+    /// <param name="diceFaces"></param>
+    /// <param name="amountOfDice"></param>
+    /// <returns>List of dice rolls</returns>
+    public IEnumerable<int> RollDice(int amountOfDice = 1, int diceFaces = 6)
+    {
+        for (var i = 0; i < amountOfDice; i++)
+            yield return RollDie(diceFaces);
+    }
 
-     }
-     
+    /// <summary>
+    /// Convert a hitpoint dice string to an integer using Regular Expressions.
+    /// </summary>
+    /// <param name="diceHitpointString">expect a string structured as 1d20 or 1d20+10</param>
+    /// <returns>summed amount of hitpoint dice</returns>
+    public int ConvertHitpointDiceToIntegerAndSum(string diceHitpointString) 
+    {
+        /* The following string: (\d+)d(\d+)([\+-]\d+)?$
+        * is a Regular Expression.
+        *
+        * The string is breaking down a hitpoint dice value into its bare components.
+        * For example:
+        *       We receive a string "1d20+10"
+        *       We call Regex.Match() and feed it our string and regular expression pattern
+        *       The string is broken down into 4 groups as such:
+        *           This breakdown gives a detailed visual view of how the groups of the
+        *           regular expression work and how they divide for the Regex.Match function:
+        *
+        *                   Grouping for breakdown: (1)d(20)((+)(10))
+        *                   All values are stored as a string.
+        *                   NOTE: (G1) == (Group1)
+        *
+        *                   (                G0               )
+        *                                       (     G3      )
+        *                   (G1) [ignored] (G2)   (G4)  (G5)
+        *                   (1)      d     (20) ( (+)   (10)  )
+        *
+        *        We then store this information into diceMatch
+        */
+        Match diceMatch = Regex.Match(diceHitpointString, @"(\d+)d(\d+)(([\+-])(\d+))?$");
+        
+        
+        /* 
+        This expression validates that the string we received is valid integers
+        For example: 
+        if we were to receive the following string 1d20+10 the string would be broken
+        up into multiple groups storing the values as a string.
+        
+        For reference the string breaks down as follows:
+            RegExpression: (\d+)d(\d+)([\+-])(\d+)?$
+            (1)[d](20)(+)(10) // We ignore [d] as it is not used in the evaluation
+            where:
+                diceMatch.Groups[1].Value == "1"
+                diceMatch.Groups[2].Value == "20"
+                diceMatch.Groups[4].Value == "+"
+                diceMatch.Groups[5].Value == "10"
+        */
+        if (int.TryParse(diceMatch.Groups[1].Value, out int amountOfDice) && 
+            int.TryParse(diceMatch.Groups[2].Value, out int typeOfDie))
+        {
+            // Check our operator. We should only see + or - : Then return our modifier with sum
+            return diceMatch.Groups[4].Value is "+" or "-" &&
+                   int.TryParse(diceMatch.Groups[5].Value, out int diceValueModifier)
+                ? diceMatch.Groups[4].Value is "+"
+                    ? RollDice(amountOfDice, typeOfDie).Sum() + diceValueModifier
+                    : RollDice(amountOfDice, typeOfDie).Sum() - diceValueModifier
+                : RollDice(amountOfDice, typeOfDie).Sum();
+        }
+
+        return 0;
+    }
 }
+     
--- DragonArchiver.Tests/DiceServiceUnitTests.cs
+++ DragonArchiver.Tests/DiceServiceUnitTests.cs
@@ -1,4 +1,5 @@
-﻿using DragonArchiver.Core.Services;
+﻿using System.Collections;
+using DragonArchiver.Core.Services;
 
 namespace DragonArchiver.Tests;
 
@@ -7,31 +8,36 @@ public class DiceServiceUnitTests
     [Fact]
     public void Validate_RollDice_Is_Returning_An_Integer_Or_List()
     {
-        var diceServiceTesting = new DiceService();
+        var diceServiceTesting = new DiceService(new Random());
         
-        Assert.InRange(diceServiceTesting.RollDice(20), 1, 20);
+        foreach (var rollDie in diceServiceTesting.RollDice(20)) Assert.InRange<int>(rollDie, 1, 20);
         Assert.Collection(diceServiceTesting.RollDice(1, 1), _ => { });
     }
     
     [Fact]
     public void Validate_ConvertHitpointDiceToIntegerAndSum_Is_Properly_Returning_An_Integer()
     {
-        var diceServiceTesting = new DiceService();
+        var diceServiceTesting = new DiceService(new Random());
         const string mustReturn0 = "0d123";
         const string mustReturn1 = "1d1";
+        const string mustNotReturnNegative = "-1d1";
+        bool positiveValueReturnsTrue = diceServiceTesting.ConvertHitpointDiceToIntegerAndSum(mustNotReturnNegative) > 0;
         const string mustReturnValueBetween1And20 = "1d20";
         const string mustReturnValueBetween10And200 = "10d20";
         const string mustReturnValueBetween1And100 = "1d100";
         const string mustReturnValueBetween1And10000 = "1d10000";
         const string mustReturnValueBetween100And100000 = "100d1000";
+        const string mustReturn11 = "1d1+10";
         
         Assert.Equal(0, diceServiceTesting.ConvertHitpointDiceToIntegerAndSum(mustReturn0));
         Assert.Equal(1, diceServiceTesting.ConvertHitpointDiceToIntegerAndSum(mustReturn1));
+        Assert.True(positiveValueReturnsTrue);
         Assert.InRange(diceServiceTesting.ConvertHitpointDiceToIntegerAndSum(mustReturnValueBetween1And20), 1, 20);
         Assert.InRange(diceServiceTesting.ConvertHitpointDiceToIntegerAndSum(mustReturnValueBetween10And200), 10, 200);
         Assert.InRange(diceServiceTesting.ConvertHitpointDiceToIntegerAndSum(mustReturnValueBetween1And100), 1, 100);
         Assert.InRange(diceServiceTesting.ConvertHitpointDiceToIntegerAndSum(mustReturnValueBetween1And10000), 1, 10000);
         Assert.InRange(diceServiceTesting.ConvertHitpointDiceToIntegerAndSum(mustReturnValueBetween100And100000), 100, 100000);
+        Assert.Equal(11, diceServiceTesting.ConvertHitpointDiceToIntegerAndSum(mustReturn11));
         
     }
 }
